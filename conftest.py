a4692c22c28e78e3f8d93fd9d783667d23be8dd8
Cyrus Afshari <cyrus.afshari@gmail.com>
2024-01-29 18:52:15 -0500

    -move fixtures to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from playwright.sync_api import Page
 
 
 @pytest.fixture(scope='session')
@@ -12,3 +13,24 @@ def browser_context_args(browser_context_args):
         'base_url': 'https://bananarepublic.gap.com'
 
     }
+
+
+def open_page(page: Page) -> None:
+    page.goto("/browse/product.do?pid=2666360923230")  # base url provided in pytest.ini
+
+
+def close_dynamic_modals(page: Page) -> None:
+    if page.get_by_test_id("dynamic-modal-content").is_visible():
+        print('closing promotional modal')
+        page.get_by_label("close email sign up modal").click()  # this label is used for multiple different modals
+
+    if page.get_by_label("Close Survey", exact=True).is_visible():
+        print('closing survey modal')
+        page.get_by_label("Close Survey", exact=True).click()
+
+
+# this fixture will run before every test
+@pytest.fixture(scope="function", autouse=True)
+def start(page: Page):
+    open_page(page)
+    close_dynamic_modals(page)

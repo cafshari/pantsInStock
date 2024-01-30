5e6a3e29f5d0f1b41af7376db7ca80f04ef81464
Cyrus Afshari <cyrus.afshari@gmail.com>
2024-01-29 23:08:46 -0500

    -changing to an in-stock size just to test email flow once more -added a comment

--- tests/test_brpants.py
+++ tests/test_brpants.py
@@ -16,21 +16,18 @@ def test_check_size_in_stock(page: Page):
     page.get_by_label("Size:32W", exact=False).click()
 
     # Click on length size "30L" NOTE: when out of stock, the label = "Size:30L out of stock" so using exact=False
-    page.get_by_label("Size:30L", exact=False).click()
-
+    page.get_by_label("Size:34L", exact=False).click()
 
     # Now that the desired size is selected, determine if it's in stock
     # 'Add to Bag' button will be disabled if it's not in stock NOTE: the label is a long sentence so using exact=False
-    env_file = os.getenv('GITHUB_ENV')
+    env_file = os.getenv('GITHUB_ENV')  # will result in env_file = None if run locally
     if page.get_by_label("Add to bag", exact=False).is_enabled():
         print("In stock!")
         if env_file:
             with open(env_file, "a") as myfile:
                 myfile.write(f"IS_IN_STOCK=true")
-        # assert True
     else:
         print("Out of stock!")
         if env_file:
             with open(env_file, "a") as myfile:
                 myfile.write(f"IS_IN_STOCK=false")
-        # assert False
